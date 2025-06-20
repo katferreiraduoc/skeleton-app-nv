d6e6d7e344f7d72d183cc717ffe582e72b881dc7
Kat Ferreira <kat.ferreira@duocuc.cl>
2025-06-20 16:25:57 -0400

    login and nav to home

--- SkeletonApp/src/app/pages/login/login.page.ts
+++ SkeletonApp/src/app/pages/login/login.page.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
-  standalone: false
+  standalone: false,
 })
 export class LoginPage implements OnInit {
+  usuario: string = '';
+  clave: string = '';
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  iniciosesion() {
+    if (
+      this.usuario.length >= 3 &&
+      this.usuario.length <= 8 &&
+      this.clave.length == 4
+    ) {
+      this.router.navigate(['/home']);
+    }
   }
 
+  ngOnInit() {}
 }
