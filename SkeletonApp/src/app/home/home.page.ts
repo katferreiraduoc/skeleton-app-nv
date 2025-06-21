e98d8ab94cf606b726ace1336e688c461f472e62
Kat Ferreira <kat.ferreira@duocuc.cl>
2025-06-21 13:13:09 -0400

    add animation

--- SkeletonApp/src/app/home/home.page.ts
+++ SkeletonApp/src/app/home/home.page.ts
@@ -1,6 +1,6 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AlertController, ToastController } from '@ionic/angular';
+import { AlertController, ToastController, IonInput, AnimationController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,12 +8,15 @@ import { AlertController, ToastController } from '@ionic/angular';
   standalone: false,
 })
 export class HomePage {
+  @ViewChild('nombreInput', { read: IonInput, static: false }) nombreInput!: IonInput;
+  @ViewChild('apellidoInput', { read: IonInput, static: false }) apellidoInput!: IonInput;
+
   usuario: string = '';
   nombre: string = '';
   apellido: string = '';
   fechaNacimiento!: Date;
 
-  constructor(private alertController: AlertController,private router: Router, private activedRoute: ActivatedRoute) {
+  constructor(private alertController: AlertController,private router: Router, private activedRoute: ActivatedRoute, private animationCtrl: AnimationController) {
     const nav = this.router.getCurrentNavigation();
     const state = nav?.extras.state ?? history.state;
 
@@ -32,5 +35,25 @@ export class HomePage {
 
   mostrardatos(){
     this.presentAlert("Su nombre es "+this.nombre+" "+this.apellido)
+  }
+
+  limpiardatos() {
+    this.nombre = '';
+    this.apellido = '';
+
+    // 2) luego disparas la animación en cada input
+    [ this.nombreInput, this.apellidoInput ].forEach(input => {
+      const el = input.getInputElement(); 
+      // getInputElement() retorna una Promise<HTMLElement>
+      el.then(nativeEl => {
+        this.animationCtrl
+          .create()
+          .addElement(nativeEl)
+          .duration(1000)       // 1 segundo
+          .iterations(1)        // solo una vez
+          .fromTo('transform', 'translateX(0)', 'translateX(30px)')
+          .play();
+      });
+    });
   }
 }
