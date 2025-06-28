e6582520e8905c3b3c58da1d518d87a706ebca5e
Kat Ferreira <kat.ferreira@duocuc.cl>
2025-06-28 12:49:40 -0400

    fix components & add functionality

--- SkeletonApp/src/app/components/certificaciones/certificaciones.component.ts
+++ SkeletonApp/src/app/components/certificaciones/certificaciones.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-certificaciones',
   templateUrl: './certificaciones.component.html',
   styleUrls: ['./certificaciones.component.scss'],
-  standalone: false
+  standalone: false,
 })
-export class CertificacionesComponent  implements OnInit {
+export class CertificacionesComponent implements OnInit {
+  nombreCertificado: string = '';
+  fechaObtencion: Date | null = new Date();
+  certificadoVence: boolean = true;
+  fechaVencimiento: Date | null = null;
 
-  constructor() { }
+  certificados: Array<{
+    nomCert: string;
+    fecObtencion: string;
+    fecVencimiento?: string;
+  }> = [];
+
+  isVisible: boolean = true;
+
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert(header: string, msj: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: msj,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  showFechaVencimiento() {
+    this.isVisible = !this.isVisible;
+  }
+
+  agregarCertificacion() {
+    if (!this.nombreCertificado || !this.fechaObtencion) {
+      this.presentAlert(
+        'Atención',
+        'Debe completar los datos para poder agregarlos.'
+      );
+    } else {
+      const nueva = {
+        nomCert: this.nombreCertificado,
+        fecObtencion: this.fechaObtencion?.toLocaleDateString() ?? '',
+        fecVencimiento: this.certificadoVence
+          ? 'Sin Vencimiento'
+          : this.fechaVencimiento?.toLocaleDateString() ?? '',
+      };
+
+      this.certificados.push(nueva);
+
+      this.nombreCertificado = '';
+      this.fechaObtencion = null;
+      this.certificadoVence = false;
+      this.fechaVencimiento = null;
+    }
+  }
+
+  eliminarCertificacion(idx: number) {
+    this.certificados.splice(idx, 1);
+  }
 
   ngOnInit() {}
-
 }
--- SkeletonApp/src/app/components/experiencia-laboral/experiencia-laboral.component.ts
+++ SkeletonApp/src/app/components/experiencia-laboral/experiencia-laboral.component.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-experiencia-laboral',
   templateUrl: './experiencia-laboral.component.html',
   styleUrls: ['./experiencia-laboral.component.scss'],
-  standalone: false
+  standalone: false,
 })
-export class ExperienciaLaboralComponent  implements OnInit {
+export class ExperienciaLaboralComponent implements OnInit {
+  nombreEmpresa: string = '';
+  fechaInicio: Date | null = new Date();
+  empleoActual: boolean = false;
+  fechaFin: Date | null = null;
+  cargo: string = '';
+  experiencia: any = [];
 
-  constructor() { }
+  experiencias: Array<{
+    nomEmpresa: string;
+    fecInicio: string;
+    fecFin?: string;
+    empleoActual: boolean;
+    cargo: string;
+  }> = [];
+
+  isVisible: boolean = true;
+
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert(header: string, msj: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: msj,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  showFechaFin() {
+    this.isVisible = !this.isVisible;
+  }
+
+  agregarExperiencia() {
+    if (!this.nombreEmpresa || !this.fechaInicio || !this.cargo) {
+      this.presentAlert(
+        'Atención',
+        'Debe completar los datos para poder agregarlos.'
+      );
+    } else {
+      const nueva = {
+        nomEmpresa: this.nombreEmpresa,
+        fecInicio: this.fechaInicio?.toLocaleDateString() ?? '',
+        fecFin: this.empleoActual
+          ? 'Presente'
+          : this.fechaFin?.toLocaleDateString() ?? '',
+        empleoActual: this.empleoActual,
+        cargo: this.cargo,
+      };
+
+      this.experiencias.push(nueva);
+
+      this.nombreEmpresa = '';
+      this.fechaInicio = null;
+      this.empleoActual = false;
+      this.fechaFin = null;
+      this.cargo = '';
+      this.isVisible = false;
+    }
+  }
+
+  eliminarExperiencia(idx: number) {
+    this.experiencias.splice(idx, 1);
+  }
 
   ngOnInit() {}
-
 }
