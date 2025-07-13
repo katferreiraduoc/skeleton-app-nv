54f765cff718a343a75212c74c5618ea644dc5ef
Kat Ferreira <kat.ferreira@duocuc.cl>
2025-07-13 18:13:37 -0400

    update build.gradle, add environment

---
+++ SkeletonApp/src/environments/environment.android.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  platform: 'android',
+  apiUrl: 'https://api.cliente.com/android',
+  featureXEnabled: true,
+};
---
+++ SkeletonApp/src/environments/environment.ios.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  platform: 'ios',
+  apiUrl: 'https://api.cliente.com/ios',
+  featureXEnabled: true,
+};
